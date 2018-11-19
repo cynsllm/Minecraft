86c2513c227aa625346a4d7b24018e258298c8b8
cynthia <cynsellam@gmail.com>
2018-11-19 21:13:46 +0200

    cynt commit

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,14 +1,17 @@
-var matrix = [];
-for(var i=0; i<10; i++) {
-    matrix[i] = [];
-    for(var j=0; j<6; j++) {
-        matrix[i][j] = "sky";
-    }
-    for(var j=6; j<10; j++){
-        matrix[i][j] = "dirt";
+function tileMap() {
+    var matrix = [];
+    for (var i = 0; i < 10; i++) {
+        matrix[i] = [];
+        for (var j = 0; j < 6; j++) {
+            matrix[i][j] = "sky";
+        }
+        for (var j = 6; j < 10; j++) {
+            matrix[i][j] = "dirt";
+        }
     }
 }
-
-if (array[j][j] === "sky") {
-    $('#container').append('<div class="sky"></div>');
-} 
+tileMap(); 
+ 
+//if (matrix[j][j] === "sky") {
+//    $('#container').append('<div class="sky"></div>');
+//} 
