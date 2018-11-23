be7fa60d8faa12a36e1d539b51cbc266c7be051c
cynthia <cynsellam@gmail.com>
2018-11-23 10:24:47 +0200

    clear code

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    axe();
-    pickaxe();
-    shovel();
+    toolSelection();
+    clickedTile();
+    currentInventory();
 });
 
 //matrix
@@ -54,136 +54,115 @@ var inventory = $("<div/>");
 inventory.attr("id", "inventory");
 $(".navbar").append(inventory);
 
-
+//var definitions and class assignments
 var axeTool = $("button:nth-child(1)").addClass("axe");
 var pickaxeTool = $("button:nth-child(2)").addClass("pickaxe");
 var shovelTool = $("button:nth-child(3)").addClass("shovel");
-axeTool.addClass("axe");
-pickaxeTool.addClass("pickaxe");
-shovelTool.addClass("shovel");
 
-
-
-/////
-
+//function that defines the class of the selected tool
 var tool = "";
-$(".tool").on("click", function () {
-    if ($(this).hasClass("axe")) {
-        tool = "axe";
-    }
-    else if ($(this).hasClass("pickaxe")) {
-        tool = "pickaxe";
-    }
-    else {
-        tool = "shovel";
-    }
-});
+var toolSelection = function () {
+    $(".tool").on("click", function () {
+        if ($(this).hasClass("axe")) {
+            tool = "axe";
+        }
+        else if ($(this).hasClass("pickaxe")) {
+            tool = "pickaxe";
+        }
+        else {
+            tool = "shovel";
+        }
+    });
+}
 
-
-$(".tile").on("click", function () {
-    if ($(this).hasClass("rock") && tool === "pickaxe") {
-        $(this).removeClass();
-        $(this).addClass("sky tile");
-        $("#inventory").removeClass();
-        $("#inventory").addClass("rock");
-    }
-    else if(($(this).hasClass("wood") || $(this).hasClass("dirt") || $(this).hasClass("grass") || $(this).hasClass("leaf")) && tool === "pickaxe"){
-        $(".wood, .dirt, .grass, .leaf").mousedown(function () {
-            $(".pickaxe").css("background-color", "red");
-        })
-        $(".wood, .dirt, .grass, .leaf").mouseup(function () {
-            $(".pickaxe").css("background-color", "black");
-        })
-    }
-    else if ($(this).hasClass("grass") && tool === "shovel") {
-        $(this).removeClass();
-        $(this).addClass("sky tile");
-        $("#inventory").removeClass();
-        $("#inventory").addClass("grass");
-    }
-    else if ($(this).hasClass("dirt") && tool === "shovel"){
-        $(this).removeClass();
-        $(this).addClass("sky tile");
-        $("#inventory").removeClass();
-        $("#inventory").addClass("dirt");
-    }
-    else if (($(this).hasClass("rock") || $(this).hasClass("wood") || $(this).hasClass("leaf")) && tool === "shovel") {
-        $(".rock, .wood, .leaf").mousedown(function () {
-            $(".shovel").css("background-color", "red");
-        })
-        $(".rock, .wood, .leaf").mouseup(function () {
-            $(".shovel").css("background-color", "black");
-        })
-    }
-    else if ($(this).hasClass("leaf") && tool === "axe") {
-        $(this).removeClass();
-        $(this).addClass("sky tile");
-        $("#inventory").addClass("leaf");
-    }
-    else if($(this).hasClass("wood") && tool === "axe"){
-        $(this).removeClass();
-        $(this).addClass("sky tile");
-        $("#inventory").removeClass();
-        $("#inventory").addClass("wood");
-    }
-    else if (($(this).hasClass("rock") || $(this).hasClass("grass") || $(this).hasClass("dirt")) && tool === "axe"){
-        $(".rock, .grass, .dirt").mousedown(function () {
-            $(".axe").css("background-color", "red");
-        })
-        $("rock, .grass, .dirt").mouseup(function () {
-            $(".axe").css("background-color", "black");
-        })
-    }
-    else if ($(this).hasClass("sky") && inventory === "grass") {
-        $(this).removeClass();
-        $(this).addClass("grass tile");
-        inventory = "";
-        $("#inventory").removeClass()
-       
-    }
-    else if ($(this).hasClass("sky") && inventory === "dirt") {
-        $(this).removeClass();
-        $(this).addClass("dirt tile");
-        inventory = "";
-        $("#inventory").removeClass()
-        
-    }
-    else if ($(this).hasClass("sky") && inventory === "rock") {
-        $(this).removeClass();
-        $(this).addClass("rock tile");
-        inventory = "";
-        $("#inventory").removeClass()
-    }
-    else if ($(this).hasClass("sky") && inventory === "leaf") {
-        $(this).removeClass();
-        $(this).addClass("leaf tile");
-        inventory = "";
-        $("#inventory").removeClass()
-    }
-    else if ($(this).hasClass("sky") && inventory === "wood") {
-        $(this).removeClass();
-        $(this).addClass("wood tile");
-        inventory = "";
-        $("#inventory").removeClass()
-    }
-})
-
+//function that defines the class of the inventory 
 var inventory = "";
-$("#inventory").on("click", function () {
-    if ($(this).hasClass("grass")) {
-        inventory = "grass";
-    }
-    else if ($(this).hasClass("dirt")) {
-        inventory = "dirt";
-    }
-    else if ($(this).hasClass("rock")) {
-        inventory = "rock";
-    }
-    else if ($(this).hasClass("leaf")) {
-        inventory = "leaf";
-    }
-    else if ($(this).hasClass("wood")) {
-        inventory = "wood";
-    }
-})
+var currentInventory = function () {
+    $("#inventory").on("click", function () {
+        if ($(this).hasClass("grass")) {
+            inventory = "grass";
+        }
+        else if ($(this).hasClass("dirt")) {
+            inventory = "dirt";
+        }
+        else if ($(this).hasClass("rock")) {
+            inventory = "rock";
+        }
+        else if ($(this).hasClass("leaf")) {
+            inventory = "leaf";
+        }
+        else if ($(this).hasClass("wood")) {
+            inventory = "wood";
+        }
+    })
+}
+
+//function that defines an action according to the selected tool and the selected tile
+var clickedTile = function () {
+    $(".tile").on("click", function () {
+        if ($(this).hasClass("rock") && tool === "pickaxe") {
+            $(this).removeClass();
+            $(this).addClass("sky tile");
+            $("#inventory").removeClass();
+            $("#inventory").addClass("rock");
+        }
+        else if ($(this).hasClass("grass") && tool === "shovel") {
+            $(this).removeClass();
+            $(this).addClass("sky tile");
+            $("#inventory").removeClass();
+            $("#inventory").addClass("grass");
+        }
+        else if ($(this).hasClass("dirt") && tool === "shovel") {
+            $(this).removeClass();
+            $(this).addClass("sky tile");
+            $("#inventory").removeClass();
+            $("#inventory").addClass("dirt");
+        }
+        else if ($(this).hasClass("leaf") && tool === "axe") {
+            $(this).removeClass();
+            $(this).addClass("sky tile");
+            $("#inventory").removeClass();
+            $("#inventory").addClass("leaf");
+        }
+        else if ($(this).hasClass("wood") && tool === "axe") {
+            $(this).removeClass();
+            $(this).addClass("sky tile");
+            $("#inventory").removeClass();
+            $("#inventory").addClass("wood");
+        }
+        else if ($(this).hasClass("sky") && inventory === "grass") {
+            $(this).removeClass();
+            $(this).addClass("grass tile");
+            inventory = "";
+            $("#inventory").removeClass()
+        }
+        else if ($(this).hasClass("sky") && inventory === "dirt") {
+            $(this).removeClass();
+            $(this).addClass("dirt tile");
+            inventory = "";
+            $("#inventory").removeClass()
+
+        }
+        else if ($(this).hasClass("sky") && inventory === "rock") {
+            $(this).removeClass();
+            $(this).addClass("rock tile");
+            inventory = "";
+            $("#inventory").removeClass()
+        }
+        else if ($(this).hasClass("sky") && inventory === "leaf") {
+            $(this).removeClass();
+            $(this).addClass("leaf tile");
+            inventory = "";
+            $("#inventory").removeClass()
+        }
+        else if ($(this).hasClass("sky") && inventory === "wood") {
+            $(this).removeClass();
+            $(this).addClass("wood tile");
+            inventory = "";
+            $("#inventory").removeClass()
+        }
+    })
+}
+
+
 
